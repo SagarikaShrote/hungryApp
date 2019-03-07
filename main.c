65882c017a471b1648d689ec1fd9509e01ca7ae5
SagarikaShrote <42342995+SagarikaShrote@users.noreply.github.com>
2019-03-08 00:34:27 +0530

    Create main.c
    contains main methos
    yet to be completed
    call to fn pending

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+    int choice,flag=1;
+    while(flag==1){
+        printf("give your choice\n");
+        printf("1.\n");
+        scanf("%d",&choice);
+        switch(choice){
+
+
+        }
+        printf("\nto continue press 1 or else press 0");
+        scanf("%d",&flag);
+    }
+}
+
