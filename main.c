b375205fb27c4b96cd184c739b4dca73dc97eaac
Sanket Gajbhiye <40202820+Sank1411@users.noreply.github.com>
2019-03-08 23:02:29 +0530

    Update main.c
    Updated

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
-#include<stdio.h>
-
-main(){
-    int choice,flag=1;
-    while(flag==1){
-        printf("give your choice\n");
-        printf("1.\n");
-        scanf("%d",&choice);
-        switch(choice){
-
-
-        }
-        printf("\nto continue press 1 or else press 0");
-        scanf("%d",&flag);
-    }
+int main()
+{
+	int i;
+	printf("1.Search Restaurant\n");
+	printf("2.Add Restaurant\n");
+	printf("3.Order from Restaurant\n");
+	printf("4.Details of order\n");
+	while(true)
+	{
+		scanf("%d",&i);
+		switch(i)
+		{
+			case 1: search();
+			break;
+			case 2: Add();
+			break;
+			case 3: order();
+			break;
+			case 4: details();
+			break;
+			default: break;
+		}
+	}
+	return 0;
 }
-
